5c25f67abd79f48ebd043a7e9d77e3cecc62845d
Lunarmo <lunarmohamd@gmail.com>
2026-08-15 18:38:24 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // هذا هو الـ Publishable/Anon key المخصص للواجهة، وليس service_role secret.
 window.LUNA_CONFIG = {
   SUPABASE_URL: "https://wdjsefcbzcxcblkyhppf.supabase.co",
-  SUPABASE_ANON_KEY: "ضع_هنا_الـ_PUBLISHABLE_KEY"
+  SUPABASE_ANON_KEY: "sb_publishable_7Sa2evuhMMy2KNXEfMJ3cg_yUIr0XI0"
 };
