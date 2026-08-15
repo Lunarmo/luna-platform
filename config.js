b3aac9523ae31b1bcd8322cfcebf7d6f1dba06a4
Lunarmo <lunarmohamd@gmail.com>
2026-08-15 18:39:32 +0300

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// ضع مفتاح Supabase العام هنا فقط.
-// هذا هو الـ Publishable/Anon key المخصص للواجهة، وليس service_role secret.
-window.LUNA_CONFIG = {
-  SUPABASE_URL: "https://wdjsefcbzcxcblkyhppf.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_7Sa2evuhMMy2KNXEfMJ3cg_yUIr0XI0"
-};
